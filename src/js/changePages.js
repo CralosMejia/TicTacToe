a99dd3082d73d891cd790d883d552812190c76a4
Carlos Mejia <camh670@hotmail.com>
2021-09-24 13:34:57 -0500

    Termine el responsive de ambas paginas

--- src/js/changePages.js
+++ src/js/changePages.js
@@ -1,20 +1,35 @@
 const btnPlayerOne=document.getElementById("button-one-player");
 const btnPlayerTwo=document.getElementById("button-two-player");
 const btnBack=document.getElementById("button-back-id");
+const labelPlayer2=document.querySelector(".Player2");
 
-let typeOfGame=0;
-//------Buttons 
+//-----------------Functions
+function clickXo(){
+    console.log(this+'hola');
+}
+
+function modeGame(){
+    if(typeOfGame===1){
+        labelPlayer2.innerHTML="Player Two(o)";
+    }else if(typeOfGame === 2){
+        labelPlayer2.innerHTML="PC(o)";
+    }
+}
+//--------Cambiar la pantalla
 if(btnPlayerOne !== null && btnPlayerTwo !==null){
     btnPlayerOne.addEventListener('click',()=>{
-        this.window.close();
-        window.open("../html/game.html");
         typeOfGame=1;
+        debugger;
+        window.open("../html/game.html");
+        window.close();
+        modeGame();
     });
     
     btnPlayerTwo.addEventListener('click',()=>{
+        typeOfGame=2;
         this.window.close();
         window.open("../html/game.html");
-        typeOfGame=2;
+        modeGame();
     });
 }else{
     btnBack.addEventListener('click',()=>{
@@ -23,3 +38,5 @@ if(btnPlayerOne !== null && btnPlayerTwo !==null){
         typeOfGame=0;
     });
 }
+
+//listeners botones xo
